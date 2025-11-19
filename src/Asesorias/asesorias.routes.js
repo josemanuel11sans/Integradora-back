cf10e95d0635df5e480d335b1d6e1472cfe2a161
astVVG <20233tn158@utez.edu.mx>
2025-11-19 01:34:03 -0600

    [UPDATED]: Consultas en asesorias por: {tutor, materia, carrera}

--- src/Asesorias/asesorias.routes.js
+++ src/Asesorias/asesorias.routes.js
@@ -8,157 +8,21 @@ const asesoriaController = require('./asesorias.controller');
 const authenticate = require('../auth/middlewares/authenticate');
 const authorizeRoles = require('../auth/middlewares/authorizeRoles');
 
-///ducumetacion 
-/**
- * @swagger
- * tags:
- *   name: asesorias
- *   description: Endpoints para gestionar asesorias
- */
-
-
-/**
- * @swagger
- * /api/asesorias/all:
- *   get:
- *     summary: Obtener todos los asesorias
- *     tags: [asesorias]
- *     security:
- *       - bearerAuth: []
- *     description: Retorna la lista completa de asesorias. Solo accesible por admin y coordinador.
- *     responses:
- *       200:
- *         description: Lista de asesorias obtenida con éxito.
- *       401:
- *         description: No autenticado.
- *       403:
- *         description: No autorizado.
- */
-router.get('/asesorias/all',authenticate, authorizeRoles('admin','coordinador'), asesoriaController.list); 
-// Ruta GET para obtener todos los asesorias
-
-/**
- * @swagger
- * /api/asesorias/id/{id}:
- *   get:
- *     summary: Obtener un asesoria por ID
- *     tags: [asesorias]
- *     security:
- *       - bearerAuth: []
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: integer
- *         description: ID del asesoria
- *     responses:
- *       200:
- *         description: asesoria encontrado
- *       404:
- *         description: asesoria no encontrado
- *       401:
- *         description: No autenticado
- *       403:
- *         description: No autorizado
- */
-router.get('/asesorias/id/:id',authenticate,authorizeRoles('coordinador'), asesoriaController.getOne); 
-// Ruta GET para obtener un asesoria por id
-
-/**
- * @swagger
- * /api/asesorias/create:
- *   post:
- *     summary: Crear un nuevo asesoria
- *     tags: [asesorias]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               nombre:
- *                 type: string
- *               correo:
- *                 type: string
- *               password:
- *                 type: string
- *               rol:
- *                 type: string
- *     responses:
- *       201:
- *         description: asesoria creado correctamente
- *       400:
- *         description: Datos inválidos
- */
-router.post('/asesorias/create', asesoriaController.create); 
-// Ruta POST para crear un nuevo asesoria
-
-/**
- * @swagger
- * /api/asesorias/update/{id}:
- *   put:
- *     summary: Actualizar un asesoria
- *     tags: [asesorias]
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: integer
- *         description: ID del asesoria a actualizar
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               nombre:
- *                 type: string
- *               correo:
- *                 type: string
- *               rol:
- *                 type: string
- *     responses:
- *       200:
- *         description: asesoria actualizado exitosamente
- *       404:
- *         description: asesoria no encontrado
- */
-router.put('/asesorias/update/:id', asesoriaController.update); 
-// Ruta PUT para actualizar un asesoria por id
-
-/**
- * @swagger
- * /api/asesorias/delete/{id}:
- *   delete:
- *     summary: Eliminar un asesoria por ID
- *     tags: [asesorias]
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: integer
- *     responses:
- *       200:
- *         description: asesoria eliminado correctamente
- *       404:
- *         description: asesoria no encontrado
- */
-router.delete('/asesorias/delete/:id', asesoriaController.remove); 
-// Ruta DELETE para eliminar un asesoria por id
-
-// Consultas especificas
-// ------------Estudiante-------------
 
 // Crear una asesoria
-router.post('/student/:estudiante_id/create', asesoriaController.createAsesoriaByStudent);
+router.post('/student/:estudiante_id/create', asesoriaController.create);
 
 // Obtener asesorias por estudiante
-router.get('/student/:estudiante_id', asesoriaController.getAsesoriasByStudent);
+router.get('/student/:estudiante_id', asesoriaController.getByStudent);
+
+// Obtener asesorias por tutor
+router.get('/tutor/:tutor_id', asesoriaController.getByTutor);
+
+// Obtener asesorias por materia
+router.get('/materia/:materia_id', asesoriaController.getByMateria);
+
+// Obtener asesorias por carrera
+router.get('/carrera/:carrera_id', asesoriaController.getByCarrera);
 
 module.exports = router; 
 // Exporta el router para usarlo en la app principal
--- src/Asesorias/asesorias.service.js
+++ src/Asesorias/asesorias.service.js
@@ -15,10 +15,10 @@ const getById = async (id) => {
   // Busca un Asesoria por su primary key
 };
 
-const createAsesoria = async (data) => {
-  return await Asesoria.create(data); 
-  // Crea un nuevo Asesoria con los datos proporcionados
-};
+// const createAsesoria = async (data) => {
+//   return await Asesoria.create(data); 
+//   // Crea un nuevo Asesoria con los datos proporcionados
+// };
 
 const updateAsesoria = async (id, data) => {
   const Asesoria = await Asesoria.findByPk(id); 
@@ -42,19 +42,27 @@ const deleteAsesoria = async (id) => {
 
 
 // Consultas especificas
-// ------------Estudiante-------------
 
-// Crear una asesoria
-const createAsesoriaByStudent = async (estudiante_id, data) => {
+/*
+Crear asesoria
+Roles: student, coordinador
+FPKs: estudiante_id, tutor_id, espacio_id, carrera_id, materia_id
+Otros: comentarios, fecha_asesoria, asistencia (default false)
+*/
+const createAsesoria = async (studentId, data) => {
   return await Asesoria.create({
     ...data,                 // comentarios, tutor_id, espacio_id, fecha_asesoria, carrera_id
-    estudiante_id,           // forzamos que el id venga del params
+    estudiante_id: studentId,           // forzamos que el id venga del params
     asistencia: false        // siempre false al crear
   });
 };
 
-// Obtener asesorias por estudiante
 
+/*
+Obtener asesorias por estudiante
+Roles: student, tutor, coordinador
+FPK: estudiante_id
+*/
 const getAsesoriasByStudent = async (studentId) => {
   return await Asesoria.findAll({
     where: { estudiante_id: studentId },
@@ -63,7 +71,45 @@ const getAsesoriasByStudent = async (studentId) => {
 };
 
 
-module.exports = { getAll, getById, createAsesoria, updateAsesoria, deleteAsesoria, 
-createAsesoriaByStudent, getAsesoriasByStudent 
+/*
+Obtener asesorias por tutor
+Roles: tutor, coordinador
+FPK: tutor_id
+*/
+const getAsesoriasByTutor = async (tutorId) => {
+  return await Asesoria.findAll({
+    where: { tutor_id: tutorId },
+    order: [['fecha_asesoria', 'DESC']]
+  });
+}
+
+/*
+Obtener asesorias por materia
+Roles: tutor, coordinador
+FPK: materia_id
+*/
+const getAsesoriasByMateria = async (materiaId) => {
+  return await Asesoria.findAll({
+    where: { materia_id: materiaId },
+    order: [['fecha_asesoria', 'DESC']]
+  });
+};
+
+/*
+Obtener asesorias por carrera
+Roles: tutor, coordinador
+FPK: carrera_id
+*/
+const getAsesoriasByCarrera = async (carreraId) => {
+  return await Asesoria.findAll({
+    where: { carrera_id: carreraId },
+    order: [['fecha_asesoria', 'DESC']]
+  });
+}
+
+
+module.exports = { getAll, getById, createAsesoria, updateAsesoria, deleteAsesoria,
+getAsesoriasByStudent, getAsesoriasByTutor, getAsesoriasByMateria,
+getAsesoriasByCarrera,
 }; 
 // Exporta todas las funciones del servicio
