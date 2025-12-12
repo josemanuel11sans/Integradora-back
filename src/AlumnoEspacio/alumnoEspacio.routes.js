7734f2878270c4ce3aa4b91fb174cf905b713876
josemanuel11sans <20233tn078@utez.edu.mx>
2025-12-12 02:37:56 -0600

    espacios

--- src/AlumnoEspacio/alumnoEspacio.routes.js
+++ src/AlumnoEspacio/alumnoEspacio.routes.js
@@ -10,40 +10,31 @@ const authorizeRoles = require("../auth/middlewares/authorizeRoles");
 // Inscribir alumno
 router.post(
   "/:espacioId/alumnos/:alumnoId",
-  authenticate,
-  authorizeRoles("student", "coordinator", "admin"),
   controller.inscribir
 );
 
 // Desinscribir alumno
 router.delete(
   "/:espacioId/alumnos/:alumnoId",
-  authenticate,
-  authorizeRoles("student", "coordinator", "admin"),
   controller.desinscribir
 );
 
 // Listar alumnos del espacio
 router.get(
   "/:espacioId/alumnos",
-  authenticate,
-  authorizeRoles("tutor", "coordinator", "admin"),
   controller.alumnosDeEspacio
 );
 
 // Listar espacios del alumno
 router.get(
   "/alumno/:alumnoId",
-  authenticate,
-  authorizeRoles("student", "tutor", "coordinator", "admin"),
+
   controller.espaciosDeAlumno
 );
 
 
 router.get(
   "/alumno/:alumnoId/carrera",
-  authenticate,
-  authorizeRoles("student", "coordinator", "admin"),
   controller.getEspaciosPorCarreraDeAlumno
 );
 
